d24163b33cd964500b20ba7811802ac640161001
ikjangid <kjangid@hotmail.com>
2020-10-10 11:04:35 +0530

    Added ViewSet to user profile api

--- UserProfiles/user_profile_api/urls.py
+++ UserProfiles/user_profile_api/urls.py
@@ -1,7 +1,14 @@
-from django.urls import path
+from django.urls import path, include
+
+
+from rest_framework.routers import DefaultRouter
 
 from user_profile_api import views
 
+router = DefaultRouter()
+router.register('my-viewset', views.MyViewSet, basename='my-viewset')
+
 urlpatterns = [
     path('api-view/', views.MyApiView.as_view()),
+    path('', include(router.urls))    
 ]
